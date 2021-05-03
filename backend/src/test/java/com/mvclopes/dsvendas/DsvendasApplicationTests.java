b5a62ceb1e4fcaa5ab74ad9967bd93b3532f5cd7
Matheus Lopes <matheus.lopes23@hotmail.com>
2021-05-03 20:06:19 -0300

    Criação projeto

---
+++ backend/src/test/java/com/mvclopes/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvclopes.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
